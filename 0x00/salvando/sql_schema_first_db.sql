7caa48be5afd3e4111378ef283eb729def61a0b1
Leonardo Quadros <leonardoq@ciandt.com>
2022-06-08 15:46:42 -0300

    Banco de dados

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,13 @@
+-- TABLE
+CREATE TABLE cliente (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(80) NOT NULL,
+email VARCHAR(40) NOT NULL,
+telefone INT
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
